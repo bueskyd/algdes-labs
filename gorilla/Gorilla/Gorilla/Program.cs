af3ee96699f104024acca0882a8b72b937527328
alexander <alexander@berg.dk>
2022-10-03 11:10:53 +0200

    Solve Gorilla

--- gorilla/Gorilla/Gorilla/Program.cs
+++ gorilla/Gorilla/Gorilla/Program.cs
@@ -6,10 +6,11 @@ namespace Gorilla
     public class Gorilla
     {
         private static int[,] costs;
+        private static int gapCost;
 
         private static void ReadCosts()
         {
-            using var reader = new StreamReader("..\\..\\..\\..\\..\\data\\BLOSUM62.txt");
+            using var reader = new StreamReader("..\\..\\data\\BLOSUM62.txt");
             string line;
             while ((line = reader.ReadLine())[0] == '#') ;
             string[] chars = line.Split(
@@ -41,6 +42,7 @@ namespace Gorilla
                 for (int j = 1; j < row.Length; j++)
                     costs[chars[j - 1][0], row[0][0]] = int.Parse(row[j]);
             }
+            gapCost = costs['A', '*'];
         }
 
         class Animal
@@ -68,61 +70,59 @@ namespace Gorilla
             }
         }
 
-        struct TableEntry
-        {
-            public int m;
-            public int deltaI;
-            public int deltaJ;
-
-            public TableEntry(int m, int deltaI, int deltaJ)
-            {
-                this.m = m;
-                this.deltaI = deltaI;
-                this.deltaJ = deltaJ;
-            }
-        }
-
         private static (string, string) ConstructSolution(
-            TableEntry[,] m, string str0, string str1)
+            int[,] m, string str0, string str1)
         {
-            int x = str0.Length - 1;
-            int y = str1.Length - 1;
+            int x = str0.Length;
+            int y = str1.Length;
             StringBuilder sb1 = new();
             StringBuilder sb0 = new();
             while (x != 0 || y != 0)
             {
-                TableEntry te = m[x, y];
-                if (te.deltaI == te.deltaJ)
-                {
-                    sb0.Append(str0[x]);
-                    sb1.Append(str1[y]);
-                }
-                else if (te.deltaI == 1)
-                {
-                    sb0.Append(str0[x]);
-                    sb1.Append('-');
-                }
-                else if (te.deltaJ == 1)
+                int cost = m[x, y];
+
+                if (x == 0)
                 {
                     sb0.Append('-');
-                    sb1.Append(str1[y]);
+                    sb1.Append(str1[y--]);
                 }
-                x -= te.deltaI;
-                y -= te.deltaJ;
+                else if (y == 0)
+                {
+                    sb0.Append(str0[x--]);
+                    sb1.Append('-');
+                }
+                else if (cost ==
+                        m[x, y - 1] + gapCost)
+                {
+                    sb0.Append('-');
+                    sb1.Append(str1[--y]);
+                }
+                else if (
+                    cost ==
+                    m[x - 1, y] + gapCost)
+                {
+                    sb0.Append(str0[--x]);
+                    sb1.Append('-');
+                }
+                else if (
+                    cost == m[x - 1, y - 1] +
+                    costs[str0[x - 1], str1[y - 1]])
+                {
+                    sb0.Append(str0[--x]);
+                    sb1.Append(str1[--y]);
+                }
             }
-            sb0.Append(str0[0]);
-            sb1.Append(str1[0]);
             string result0 = new(sb0.ToString().Reverse().ToArray());
             string result1 = new(sb1.ToString().Reverse().ToArray());
             return (result0, result1);
         }
 
-        private static void Print(TableEntry[,] m, Func<TableEntry, int> f)
+        private static void Print(int[,] m)
         {
             for (int y = 0; y < m.GetLength(1); y++)
             {
                 for (int x = 0; x < m.GetLength(0); x++)
-                    Console.Write(f(m[x, y]) + "\t");
+                    Console.Write(m[x, y] + "\t");
                 Console.WriteLine();
             }
             Console.WriteLine();
@@ -130,12 +130,11 @@ namespace Gorilla
 
         private static (int, string, string) Solve(string str0, string str1)
         {
-            int gapCost = -4;
-            TableEntry[,] m = new TableEntry[str0.Length + 1, str1.Length + 1];
+            int[,] m = new int[str0.Length + 1, str1.Length + 1];
             for (int i = 1; i < str0.Length + 1; i++)
-                m[i, 0] = new TableEntry(i * gapCost, 1, 0);
+                m[i, 0] = i * gapCost;
             for (int i = 1; i < str1.Length + 1; i++)
-                m[0, i] = new TableEntry(i * gapCost, 0, 1);
+                m[0, i] = i * gapCost;
 
             for (int i = 1; i < str0.Length + 1; i++)
             {
@@ -143,9 +142,9 @@ namespace Gorilla
                 {
                     int match =
                         costs[str0[i - 1], str1[j - 1]] +
-                        m[i - 1, j - 1].m;
-                    int gap0 = gapCost + m[i - 1, j].m;
-                    int gap1 = gapCost + m[i, j - 1].m;
+                        m[i - 1, j - 1];
+                    int gap0 = gapCost + m[i - 1, j];
+                    int gap1 = gapCost + m[i, j - 1];
                     int deltaI = 0, deltaJ = 0;
                     int cost;
                     if (match >= gap0 && match >= gap1)
@@ -168,10 +167,10 @@ namespace Gorilla
                     {
                         throw new Exception("This should never happen");
                     }
-                    m[i, j] = new TableEntry(cost, deltaI, deltaJ);
+                    m[i, j] = cost;
                 }
             }
-            int minCost = m[str0.Length, str1.Length].m;
+            int minCost = m[str0.Length, str1.Length];
             var (s0, s1)= ConstructSolution(m, str0, str1);
             return (minCost, s0, s1);
         }
@@ -268,63 +267,36 @@ namespace Gorilla
                         $"Incorrect cost on test case {e.name0}--{e.name1}." +
                         $" Was {result.cost}, expected {e.cost} " +
                         $"(difference {Math.Abs(e.cost - result.cost)}).");
-                    Console.WriteLine($"Expected alignments\n{e.alignment0}\n{e.alignment1}");
-                    Console.WriteLine($"Actual alignments\n{result.alignment0}\n{result.alignment1}");
-                    if (e.alignment0 == result.alignment0)
-                        Console.WriteLine($"First alignments are equal");
-                    else
-                    {
-                        string s0 = "";
-                        string s1 = "";
-                        for (int i = 0; i < e.alignment0.Length; i++)
-                        {
-                            if (e.alignment0[i] != result.alignment0[i])
-                            {
-                                s0 += e.alignment0[i];
-                                s1 += result.alignment0[i];
-                            }
-                            else
-                            {
-                                s0 += '-';
-                                s1 += '-';
-                            }
-                        }
-                        Console.WriteLine(s0);
-                        Console.WriteLine(s1);
-                    }
-                    if (e.alignment1 == result.alignment1)
-                        Console.WriteLine($"Second alignments are equal");
-                    else
-                    {
-                        string s0 = "";
-                        string s1 = "";
-                        for (int i = 0; i < e.alignment1.Length; i++)
-                        {
-                            if (e.alignment1[i] != result.alignment1[i])
-                            {
-                                s0 += e.alignment1[i];
-                                s1 += result.alignment1[i];
-                            }
-                            else
-                            {
-                                s0 += '-';
-                                s1 += '-';
-                            }
-                        }
-                        Console.WriteLine(s0);
-                        Console.WriteLine(s1);
-                    }
-                    Console.WriteLine();
                 }
+                int resultCost = AlignmentCosts(result.alignment0, result.alignment1);
+                int expectedCost = AlignmentCosts(e.alignment0, e.alignment1);
+                if (resultCost != expectedCost)
+                    Console.WriteLine(
+                        $"Incorrect alignment costs. " +
+                        $"Expected alignment cost: {expectedCost}. Actual alignment cost {resultCost}.");
+                Console.WriteLine();
             }
+        }
+
+        private static int AlignmentCosts(string alignment0, string alignment1)
+        {
+            int cost = 0;
+            for (int i = 0; i < alignment0.Length; i++)
+            {
+                if (alignment0[i] != '-' && alignment1[i] != '-')
+                    cost += costs[alignment0[i], alignment1[i]];
+                else
+                    cost += gapCost;
+            }
+            return cost;
         }
 
         public static void Main(string[] args)
         {
             ReadCosts();
-            ReadInput(new StreamReader("..\\..\\..\\..\\..\\data\\HbB_FASTAs-in.txt"));
+            ReadInput(new StreamReader("..\\..\\data\\HbB_FASTAs-in.txt"));
             var actual = Solve();
-            var expected = ReadExpected("..\\..\\..\\..\\..\\data\\HbB_FASTAs-out.txt");
+            var expected = ReadExpected("..\\..\\data\\HbB_FASTAs-out.txt");
             CompareResults(expected, actual);
             PrintResults(actual);
         }
