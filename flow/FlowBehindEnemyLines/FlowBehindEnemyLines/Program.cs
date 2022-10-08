8b48573a3e7e7f8e2488d3ee87cfbf314bb65c41
alexander <alexander@berg.dk>
2022-10-08 16:44:45 +0200

    Test 2

--- flow/FlowBehindEnemyLines/FlowBehindEnemyLines/Program.cs
+++ flow/FlowBehindEnemyLines/FlowBehindEnemyLines/Program.cs
@@ -2,15 +2,15 @@
 {
     public class Edge
     {
-        public int other;
-        public int capacity;
+        public Edge other;
         public int from;
         public int to;
-        public bool isRecidualEdge;
+        public int id;
+        public bool isResidualEdge;
 
         public override string ToString()
         {
-            return $"({from}, {to}, {capacity}, {isRecidualEdge})";
+            return $"({from}, {to}, {isResidualEdge})";
         }
     }
 
@@ -24,6 +24,8 @@
     public class FlowBehindEnemyLines
     {
         private static Graph graph = new();
+        private static List<int> flows = new();
+        private static List<int?> capacities = new();
 
         private static void ReadInput()
         {
@@ -36,26 +38,28 @@
             int m = int.Parse(Console.ReadLine());
             for (int i = 0; i < m; i++)
             {
+                flows.Add(0);
                 string[] words = Console.ReadLine().Split(' ');
                 int a = int.Parse(words[0]);
                 int b = int.Parse(words[1]);
                 int c = int.Parse(words[2]);
+                capacities.Add(c != -1 ? c : null);
                 Edge edge = new()
                 {
-                    capacity = c == -1 ? -1 : 0,
                     from = a,
                     to = b,
-                    other = graph.adjacent[b].Count,
-                    isRecidualEdge = false
+                    id = i,
+                    isResidualEdge = false
                 };
                 Edge residual = new()
                 {
-                    capacity = c,
                     from = b,
                     to = a,
-                    other = graph.adjacent[a].Count,
-                    isRecidualEdge = true
+                    id = i,
+                    isResidualEdge = true
                 };
+                edge.other = residual;
+                residual.other = edge;
                 graph.adjacent[a].Add(edge);
                 graph.adjacent[b].Add(residual);
             }
@@ -63,9 +67,9 @@
             graph.sink = n - 1;
         }
 
-        private static Edge GetOther(Edge edge)
+        private static int Remaining(int edgeId)
         {
-            return graph.adjacent[edge.to][edge.other];
+            return (int)capacities[edgeId] - Math.Abs(flows[edgeId]);
         }
 
         private static (List<Edge>, int) FindPath()
@@ -82,49 +86,35 @@
                 var adjacent = graph.adjacent[node];
                 foreach (var edge in adjacent)
                 {
-                    if (edge.isRecidualEdge && edge.capacity == 0)
-                        continue;
-                    Edge other = GetOther(edge);
-                    if (!edge.isRecidualEdge && other.capacity == 0)
-                        continue;
                     if (from[edge.to].Item2 != -1)
+                        continue;
+                    if (capacities[edge.id] == Math.Abs(flows[edge.id]))
                         continue;
                     stack.Push(edge.to);
                     from[edge.to] = (edge, node);
                     if (edge.to == graph.sink)
-                    {
-                        stack.Clear();
-                        break;
-                    }
+                        return BuildPath(from);
                 }
             }
-            if (from[graph.sink].Item2 == -1)
-                return (null, -1);
+            return (null, -1);
+        }
+
+        private static (List<Edge>, int) BuildPath(List<(Edge, int)> from)
+        {
             List<Edge> path = new()
             {
                 from[graph.sink].Item1
             };
             int prev = from[graph.sink].Item2;
             int bottleneck = int.MaxValue;
-            if (path[0].capacity != -1)
-                if (path[0].isRecidualEdge)
-                    bottleneck = path[0].capacity;
-                else
-                    bottleneck = GetOther(path[0]).capacity;
+            if (capacities[path[0].id] is not null)
+                bottleneck = Math.Min(bottleneck, Remaining(path[0].id));
 
             while (prev != graph.source)
             {
                 Edge edge = from[prev].Item1;
-                if (edge.capacity != -1)
-                {
-                    if (edge.isRecidualEdge)
-                        bottleneck = Math.Min(bottleneck, edge.capacity);
-                    else
-                    {
-                        Edge recidual = GetOther(edge);
-                        bottleneck = Math.Min(bottleneck, recidual.capacity);
-                    }
-                }
+                if (capacities[edge.id] is not null)
+                    bottleneck = Math.Min(bottleneck, Remaining(edge.id));
                 path.Add(edge);
                 prev = from[prev].Item2;
             }
@@ -135,44 +125,32 @@
         {
             foreach (Edge edge in path)
             {
-                Edge other = GetOther(edge);
-                if (edge.isRecidualEdge)
-                {
-                    if (edge.capacity > 0)
-                    {
-                        edge.capacity -= bottleneck;
-                        graph.adjacent[edge.to][edge.other].capacity += bottleneck;
-                    }
-                }
-                else if (other.capacity > 0)
-                {
-                    edge.capacity += bottleneck;
-                    graph.adjacent[edge.to][edge.other].capacity -= bottleneck;
-                }
+                //if (capacities[edge.id] is not null)
+                    if (edge.isResidualEdge)
+                        flows[edge.id] -= bottleneck;
+                    else
+                        flows[edge.id] += bottleneck;
             }
         }
 
         private static List<Edge> FindMinCut()
         {
             List<Edge> minimumCut = new();
-            List<bool> visited = new();
+            List<(Edge, int)> from = new();
             for (int i = 0; i < graph.adjacent.Count; i++)
-                visited.Add(false);
+                from.Add((null, -1));
             Queue<int> queue = new();
             queue.Enqueue(graph.source);
             while (queue.Count > 0)
             {
                 int node = queue.Dequeue();
-                if (visited[node])
-                    continue;
-                visited[node] = true;
                 var adjacent = graph.adjacent[node];
                 foreach (Edge edge in adjacent)
                 {
-                    if (edge.isRecidualEdge)
+                    if (from[edge.to].Item2 != -1)
                         continue;
-                    Edge recidual = GetOther(edge);
-                    if (recidual.capacity == 0)
+                    from[edge.to] = (edge, node);
+                    if (Math.Abs(flows[edge.id]) == capacities[edge.id])
                         minimumCut.Add(edge);
                     else
                         queue.Enqueue(edge.to);
@@ -185,8 +163,30 @@
         {
             int maxFlow = 0;
             foreach (Edge edge in minCut)
-                maxFlow += edge.capacity;
+                maxFlow += Math.Abs(flows[edge.id]);
             return maxFlow;
+        }
+
+        private static bool IsValid()
+        {
+            for (int i = 0; i < capacities.Count; i++)
+                if (capacities[i] is not null && flows[i] > capacities[i])
+                    return false;
+            List<int> nodeFlows = new();
+            foreach (var _ in graph.adjacent)
+                nodeFlows.Add(0);
+            foreach (var adjacent in graph.adjacent)
+            {
+                foreach (var edge in adjacent)
+                {
+                    nodeFlows[edge.from] -= flows[edge.id];
+                    nodeFlows[edge.to] += flows[edge.id];
+                }
+            }
+            foreach (int flow in nodeFlows)
+                if (flow != 0)
+                    return false;
+            return true;
         }
 
         private static void Solve()
@@ -197,12 +197,14 @@
                 if (path is null)
                     break;
                 Augment(path, bottleneck);
+                if (!IsValid())
+                    throw new Exception("Help");
             }
             var minimumCut = FindMinCut();
             int maxFlow = FindMaxFlow(minimumCut);
             Console.WriteLine(maxFlow);
             foreach (Edge edge in minimumCut)
-                Console.WriteLine($"{edge.from} {edge.to} {edge.capacity}");
+                Console.WriteLine($"{edge.from} {edge.to} {capacities[edge.id]}");
         }
 
         public static void Main()
