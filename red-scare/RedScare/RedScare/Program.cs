8b6456696e9c43ecd325564c31f16bd0c54c15ec
alexander <alexander@berg.dk>
2022-11-23 14:14:42 +0100

    Fixes.

--- red-scare/RedScare/RedScare/Program.cs
+++ red-scare/RedScare/RedScare/Program.cs
@@ -46,6 +46,25 @@
         }
     }
 
+    public class FlowEdge
+    {
+        public FlowEdge other;
+        public int from;
+        public int to;
+        public int id;
+        public bool isResidualEdge;
+    }
+
+    public class FlowGraph
+    {
+        public List<List<FlowEdge>> adjacent = new();
+        public int superSink;
+        public List<int> flows = new();
+        public List<int> capacities = new();
+        public int edgeToSuperSink0;
+        public int edgeToSuperSink1;
+    }
+
     public class Edge
     {
         public int from, to;
@@ -62,6 +81,7 @@
     {
         public bool directed = false;
         public List<Node> adjacent = new();
+        public int redNodes;
     }
 
     public class RedScare
@@ -82,6 +102,7 @@
             s = line[0];
             t = line[1];
             graph = new();
+            graph.redNodes = r;
             
             for (int i = 0; i < n; i++)
             {
@@ -148,8 +169,6 @@
             return costs[tId];
         }
 
-        public bool Some() => Many() > 0;
-
         public int Few()
         {
             PriorityQueue<int, int> nodes = new();
@@ -183,10 +202,11 @@
             if (most[nodeId] != -1)
                 return most[nodeId];
             Node node = graph.adjacent[nodeId];
+            most[nodeId] = node.red ? 1 : 0;
             int max = 0;
             for (int i = 0; i < node.edges.Count; i++)
                 max = Math.Max(max, Many(node.edges[i].to, most));
-            return most[nodeId] = max + (node.red ? 1 : 0);
+            return most[nodeId] += max;
         }
 
         public int Many()
@@ -196,6 +216,193 @@
                 most[i] = -1;
             return Many(sId, most);
         }
+
+        private (List<FlowEdge>, int) BuildPath(FlowGraph graph, List<(FlowEdge, int)> from, int nodeId)
+        {
+            List<FlowEdge> path = new()
+            {
+                from[graph.superSink].Item1
+            };
+            int prev = from[nodeId].Item2;
+            int bottleneck = int.MaxValue;
+            if (graph.capacities[path[0].id] != -1)
+                bottleneck = Math.Min(bottleneck, Remaining(graph, path[0].id));
+
+            while (prev != nodeId)
+            {
+                FlowEdge edge = from[prev].Item1;
+                if (graph.capacities[edge.id] != -1)
+                    bottleneck = Math.Min(bottleneck, Remaining(graph, edge.id));
+                path.Add(edge);
+                prev = from[prev].Item2;
+            }
+            return (path, bottleneck);
+        }
+
+        private (List<FlowEdge>, int) FindPath(FlowGraph graph, int nodeId)
+        {
+            List<(FlowEdge, int)> from = new();
+            for (int i = 0; i < graph.adjacent.Count; i++)
+                from.Add((null, -1));
+            from[nodeId] = (null, nodeId);
+            Stack<int> stack = new();
+            stack.Push(nodeId);
+            while (stack.Count > 0)
+            {
+                int node = stack.Pop();
+                var adjacent = graph.adjacent[node];
+                foreach (var edge in adjacent)
+                {
+                    if (from[edge.to].Item2 != -1)
+                        continue;
+                    if (graph.capacities[edge.id] == Math.Abs(graph.flows[edge.id]))
+                        continue;
+                    stack.Push(edge.to);
+                    from[edge.to] = (edge, node);
+                    if (edge.to == graph.superSink)
+                        return BuildPath(graph, from, nodeId);
+                }
+            }
+            return (null, -1);
+        }
+
+        private int Remaining(FlowGraph graph, int edgeId)
+        {
+            return graph.capacities[edgeId] - Math.Abs(graph.flows[edgeId]);
+        }
+
+        private void Augment(FlowGraph graph, List<FlowEdge> path, int bottleneck)
+        {
+            foreach (FlowEdge edge in path)
+            {
+                if (graph.capacities[edge.id] != -1)
+                    if (edge.isResidualEdge)
+                        graph.flows[edge.id] -= bottleneck;
+                    else
+                        graph.flows[edge.id] += bottleneck;
+            }
+        }
+
+        private FlowGraph CreateFlowGraph(Graph graph)
+        {
+            //Assumes that the graph is undirected
+            var flowGraph = new FlowGraph();
+            var edgesCreated = new HashSet<(int, int)>();
+            int edgePairId = 0;
+            for (int i = 0; i < graph.adjacent.Count; i++)
+                flowGraph.adjacent.Add(new List<FlowEdge>());
+            for (int i = 0; i < graph.adjacent.Count; i++)
+            {
+                var edges = graph.adjacent[i].edges;
+                for (int j = 0; j < edges.Count; j++)
+                {
+                    if (edgesCreated.Contains((i, edges[j].to)))
+                        continue;
+                    flowGraph.flows.Add(0);
+                    flowGraph.capacities.Add(1);
+                    FlowEdge edge = new()
+                    {
+                        from = i,
+                        to = edges[j].to,
+                        id = edgePairId,
+                        isResidualEdge = false
+                    };
+                    FlowEdge residual = new()
+                    {
+                        from = edges[j].to,
+                        to = i,
+                        id = edgePairId,
+                        isResidualEdge = true
+                    };
+                    edgePairId++;
+                    edge.other = residual;
+                    residual.other = edge;
+                    flowGraph.adjacent[i].Add(edge);
+                    flowGraph.adjacent[edges[j].to].Add(residual);
+                    edgesCreated.Add((i, edges[j].to));
+                }
+            }
+            flowGraph.flows.Add(0);
+            flowGraph.capacities.Add(1);
+            flowGraph.superSink = flowGraph.adjacent.Count;
+            flowGraph.adjacent.Add(new List<FlowEdge>());
+            FlowEdge sIdToSink = new()
+            {
+                from = sId,
+                to = flowGraph.superSink,
+                id = edgePairId,
+                isResidualEdge = false
+            };
+            FlowEdge sinkToSId = new()
+            {
+                from = flowGraph.superSink,
+                to = sId,
+                id = edgePairId,
+                isResidualEdge = true
+            };
+            sIdToSink.other = sinkToSId;
+            sinkToSId.other = sIdToSink;
+            flowGraph.adjacent[sId].Add(sIdToSink);
+            flowGraph.adjacent[flowGraph.superSink].Add(sinkToSId);
+
+            flowGraph.flows.Add(0);
+            flowGraph.capacities.Add(1);
+            FlowEdge tIdToSink = new()
+            {
+                from = tId,
+                to = flowGraph.superSink,
+                id = edgePairId + 1,
+                isResidualEdge = false
+            };
+            FlowEdge sinkToTId = new()
+            {
+                from = flowGraph.superSink,
+                to = tId,
+                id = edgePairId + 1,
+                isResidualEdge = true
+            };
+            tIdToSink.other = sinkToTId;
+            sinkToTId.other = tIdToSink;
+            flowGraph.adjacent[tId].Add(tIdToSink);
+            flowGraph.adjacent[flowGraph.superSink].Add(sinkToTId);
+            flowGraph.edgeToSuperSink0 = edgePairId;
+            flowGraph.edgeToSuperSink1 = edgePairId + 1;
+            return flowGraph;
+        }
+
+        private bool UndirectedAcyclicSome(int nodeId, FlowGraph flowGraph)
+        {
+            while (true)
+            {
+                var (path, bottleneck) = FindPath(flowGraph, nodeId);
+                if (path is null)
+                    break;
+                Augment(flowGraph, path, bottleneck);
+            }
+            int flow =
+                flowGraph.flows[flowGraph.edgeToSuperSink0] +
+                flowGraph.flows[flowGraph.edgeToSuperSink1];
+            return flow == 2;
+        }
+
+        public bool UndirectedCyclicSome()
+        {
+            //Transform the graph to enable running Ford-Fulkerson.
+            //Could have been prevented by doing this when the graph is read from file.
+            var flowGraph = CreateFlowGraph(graph);
+
+            for (int i = 0; i < graph.adjacent.Count; i++)
+                if (graph.adjacent[i].red)
+                {
+                    if (UndirectedAcyclicSome(i, flowGraph))
+                        return true;
+                    for (int j = 0; j < flowGraph.flows.Count; j++)
+                        flowGraph.flows[j] = 0;
+                }
+            return false;
+        }
+
+        public bool AcyclicSome() => Many() > 0;
 
         public bool Alternate()
         {
@@ -239,17 +446,24 @@
                 using var reader = new StreamReader(file);
                 redScare.ReadInput(reader);
                 Console.WriteLine($"{Path.GetFileName(file)}:\t");
-                try
+                Console.WriteLine($"\tNone = {redScare.None()}");
+                bool isCyclic = Helpers.IsCyclic(redScare.graph);
+                if (isCyclic)
                 {
-                    Console.WriteLine($" IsCyclic: {Helpers.IsCyclic(redScare.graph)}");
+                    if (redScare.graph.directed)
+                        Console.WriteLine("\tGraph is cyclic and directed. Skipping some as it would run in non-polynomial time.");
+                    else
+                        Console.WriteLine($"\tSome = {redScare.UndirectedCyclicSome()}");
                 }
-                catch (Exception) { }
-                Console.WriteLine(
-                    $"\tNone = {redScare.None()}\n" +
-                    $"\tSome = {redScare.Some()}\n" +
-                    $"\tFew = {redScare.Few()}\n" +
-                    $"\tMany = {redScare.Many()}\n" +
-                    $"\tAlternate = {redScare.Alternate()}\n");
+                else
+                    Console.WriteLine($"\tSome = {redScare.AcyclicSome()}");
+                Console.WriteLine($"\tFew = {redScare.Few()}");
+                if (!isCyclic)
+                    Console.WriteLine($"\tMany = {redScare.Many()}");
+                else
+                    Console.WriteLine("\tGraph is cyclic. Skipping Many as it would run in non-polynomial time.");
+                Console.WriteLine($"\tAlternate = {redScare.Alternate()}");
+                Console.WriteLine();
             }
             Console.WriteLine();
 
